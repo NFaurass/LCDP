a2140567a6995dc05da981a254d945cabf445154
NFaurass <36932043+NFaurass@users.noreply.github.com>
2019-06-16 15:56:51 +0000

    Update and rename OScript2.js to AScript.js

---
+++ AScript.js
@@ -0,0 +1,1 @@
+$(function(){$("#SeasonSelector").change(function(){$(".SeasonX").hide(),$("#"+$(this).val()).show()})}),$(document).keydown(function(e){return 123!=e.keyCode&&((!e.ctrlKey||!e.shiftKey||73!=e.keyCode)&&void 0)}),$(document).on("contextmenu",function(e){e.preventDefault()});
